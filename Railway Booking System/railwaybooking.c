3a19737f1be02f1d3a09c692962f4ed6c943b0e9
08021976-22022006 <poojanas.dev@gmail.com>
2025-05-20 11:02:13 +0530

    check commit

--- Railway Booking System/railwaybooking.c
+++ Railway Booking System/railwaybooking.c
@@ -3,7 +3,16 @@
 #include<pthread.h>
 #define MAX_TICKET 5
 int available_tickets = MAX_TICKET;
+// sem_t, sem_wait, sem_trywait, sem_timedwait.
 
-void book_ticket(int user_id){
-    
+sem_t ticket_lock;
+
+void book_ticket(int user_id){ // user_id = 1001
+    sem_wait(&ticket_lock); // 0(No. of tickets) (A15)
+    if(ticket_lock < available_tickets){
+
+    }
+    else if(ticket_lock > available_tickets){
+
+    }
 }
