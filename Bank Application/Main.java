2c95de2b7dee6eed81baed2b8e1e36cbd72f7f62
08021976-22022006 <poojanas.dev@gmail.com>
2025-05-20 12:37:29 +0530

    Bank Application Completed

--- Bank Application/Main.java
+++ Bank Application/Main.java
@@ -1,19 +1,18 @@
-
-class BankAccount{
+class BankAccount {
     private int balance = 1000;
 
-    public int getBalance(){
+    public int getBalance() {
         return balance;
     }
 
-    public void setBalance(int balance){
-        this.balance = balanace;
+    public void setBalance(int balance) {
+        this.balance = balance;
     }
-    
-    public void withdraw(int amount){
-        if(amount>balance){
+
+    public synchronized void withdraw(int amount) {
+        if (amount > balance) {
             System.out.println("Insufficient balance!");
-        } else{
+        } else {
             System.out.println("Withdrawn Successfully");
             balance -= amount;
             System.out.println("Remaining Balance: " + balance);
@@ -21,11 +20,12 @@ class BankAccount{
     }
 }
 
-class ATM extends Thread{
+class ATM extends Thread {
     private String username;
     private long acct_no;
     private BankAccount account;
     private int amount;
+
     public ATM(String username, long acct_no, BankAccount account, int amount) {
         this.username = username;
         this.acct_no = acct_no;
@@ -33,14 +33,20 @@ class ATM extends Thread{
         this.amount = amount;
     }
 
-    public void run(){
+    public void run() {
         account.withdraw(this.amount);
     }
-
 }
 
-public class Main{
-    public static void main(String[] args){
+public class Main {
+    public static void main(String[] args) {
+        BankAccount acct = new BankAccount();
+        ATM atm1 = new ATM("user1", 1001, acct, 500);
+        ATM atm2 = new ATM("user2", 1002, acct, 700);
+        ATM atm3 = new ATM("user3", 1003, acct, 1500);
 
+        atm1.start();
+        atm2.start();
+        atm3.start();
     }
 }
